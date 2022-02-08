1f7dd58e4e965c844e2e2f2cb51e6ab246abcbc9
keymemory <keymemory@naver.com>
2022-02-08 17:56:08 +0900

    update

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -3,12 +3,24 @@ from re import A
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 import config
 
+
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+
 # 전역변수로 선언하여 다른 함수에서도 자유롭게 사용 가능
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
@@ -17,14 +29,20 @@ def create_app():
     # ORM
     # 초기화 시키기
     db.init_app(app)
-    migrate.init_app(app, db)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
     from . import models
 
+
     # blueprint
-    from .views import main_views, question_views, answer_views
+    from .views import main_views, question_views, answer_views, auth_views, comment_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
+    app.register_blueprint(comment_views.bp)
     app.config['SECRET_KEY'] = 'any secret string'
 
     # 필터
